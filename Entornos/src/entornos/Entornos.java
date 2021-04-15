7666480df746ca59daae64a2d2ec3163198aef02
Manu <Manu Barrios@A216-06>
2021-04-15 12:22:20 +0200

    Mi primera version
    Pronto empezaré a actualizar la aplicación.
    De momento ,solo hay un mensaje por pantalla.
    
    
    Signed-off-by: Manu <Manu Barrios@A216-06>

---
+++ Entornos/src/entornos/Entornos.java
@@ -0,0 +1,10 @@
+package entornos;
+
+public class Entornos {
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+
+	}
+
+}
---
+++ Entornos/src/module-info.java
@@ -0,0 +1,3 @@
+module Entornos {
+	exports entornos;
+}
